830a7071f683e3ad27232268eef1100674c21c57
ebrahim78652 <ebrahimkarjatwala@gmail.com>
2022-06-03 19:38:41 +0200

    chore

--- redux-practice/redux-demo.js
+++ redux-practice/redux-demo.js
@@ -1,7 +1,15 @@
 const redux = require("redux");
+const bindActionCreators = redux.bindActionCreators;
+const immer = require("immer");
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCK = "CAKE_RESTOCK";
+
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCK = "ICECREAM_RESTOCK";
+
+const numInitialCakes = 10;
+const numInitialIcecreams = 20;
 
 const orderCake = () => {
   return {
@@ -16,44 +24,95 @@ const restockCake = (quantity) => {
   };
 };
 
-//implememnt the initial state
-initialState = {
-  numCakes: 10,
+const orderIcecream = () => {
+  return {
+    type: ICECREAM_ORDERED,
+  };
 };
 
-//implement the reducer
-const reducer = (state = initialState, actionObj) => {
+const restockIcecream = (quantity) => {
+  return {
+    type: ICECREAM_RESTOCK,
+    payload: quantity,
+  };
+};
+
+//implement the initial state for cakes
+const initialCakes = {
+  numCakes: numInitialCakes,
+};
+//implememnt the initial state for icecreams
+const initialIcecreams = {
+  numIceCreams: numInitialIcecreams,
+};
+
+//implement the reducer for cakes
+const cakeReducer = (state = initialCakes, actionObj) => {
   switch (actionObj.type) {
     case CAKE_ORDERED:
-      return { ...state, numCakes: state.numCakes - 1 };
+      return immer.produce(state, (draft) => {
+        draft.numCakes = draft.numCakes - 1;
+      });
     case CAKE_RESTOCK:
-      return { ...state, numCakes: state.numCakes + actionObj.payload };
+      return immer.produce(state, (draft) => {
+        draft.numCakes = draft.numCakes + actionObj.payload;
+      });
+    default:
+      return state;
+  }
+};
+//implement the reducer for ICCREAMS
+const iceCreamReducer = (state = initialIcecreams, actionObj) => {
+  switch (actionObj.type) {
+    case ICECREAM_ORDERED:
+      return immer.produce(state, (draft) => {
+        draft.numIceCreams = draft.numIceCreams - 1;
+      });
+    case ICECREAM_RESTOCK:
+      return immer.produce(state, (draft) => {
+        draft.numIceCreams = draft.numIceCreams + actionObj.payload;
+      });
     default:
       return state;
   }
 };
 
+//combine the reducers:
+const rootReducer = redux.combineReducers({
+  cake: cakeReducer,
+  icecream: iceCreamReducer,
+});
+
 //start by making a store.
-const store = redux.createStore(reducer);
+const store = redux.createStore(rootReducer);
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIcecream, restockIcecream },
+  store.dispatch
+);
 
 const unsubscribe = store.subscribe(() => {
   console.log("do some action after the update has happened!");
-  console.log(`the initial state is: ${store.getState().numCakes}`);
+  console.log(
+    `the initial state of the cakes is: ${store.getState().cake.numCakes}`
+  );
+
+  console.log(
+    `the initial state of the ice creams is: ${
+      store.getState().icecream.numIceCreams
+    }`
+  );
 });
 
-const unsubscribe2 = store.subscribe(() => {
-  console.log("COMPLETELY OTHER ACTION YOLOLOLO");
-  console.log(`the initial state is: ${store.getState().numCakes}`);
-});
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
+actions.restockCake(3);
 
-store.dispatch(restockCake(3));
+actions.orderIcecream();
+actions.orderIcecream();
+actions.orderIcecream();
+actions.orderIcecream();
+actions.orderIcecream();
 
-unsubscribe();
-
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
+actions.restockIcecream(45);
