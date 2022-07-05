4a85963c52f391ddcea5718d7c775a4af69c4ba9
ebrahim78652 <ebrahimkarjatwala@gmail.com>
2022-07-05 12:20:37 +0200

    polling with enabled option

--- reactQuery/practice/src/Components/RQSuperHeroes.page.tsx
+++ reactQuery/practice/src/Components/RQSuperHeroes.page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import axios from "axios";
 import { idText } from 'typescript';
@@ -16,9 +16,19 @@ const fetchUsers = async () => {
 };
 
 export const RQSuperHeroesPage = () => {
+
+  const [isEnabled, setEnabled]= useState(false);
   const { isLoading, error, data, isFetching, isError, refetch } = useQuery("superheroes_names",fetchUsers, {
     staleTime: 5 * 1000,
-    enabled: false, 
+    refetchInterval: (data: undefined|superHero[])=>{
+      if(data!==undefined && data.length===4){
+        setEnabled(false);
+        return false
+      }
+      
+      return 1000;
+    }, 
+    enabled: isEnabled
   });
 
 
@@ -40,7 +50,9 @@ export const RQSuperHeroesPage = () => {
             return <div key={element.id}>{element.name}</div>
           })}
         </div>}
-        <button onClick={()=>{refetch()}}>Fetch heroes</button>
+        <button onClick={()=>{
+          setEnabled(true);
+          refetch()}}>Fetch heroes</button>
       </>
     )
   }
